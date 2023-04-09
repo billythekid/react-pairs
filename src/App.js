5722cb0f9c297729f8480ae482625655096c13e9
billythekid <billythekid@github.com>
2023-04-09 03:51:19 +0100

    Added the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Deck, { PlayingCard } from "./Deck";
+import Card from "./Card";
+import Confetti from 'react-confetti';
+import Modal from 'react-modal';
 
 function App() {
+  const [attempts, setAttempts] = React.useState(0);
+  const [matches, setMatches] = React.useState(0);
+  const [matchedCards, setMatchedCards] = React.useState(new Set());
+  const [attemptCards, setAttemptCards] = React.useState(new Set());
+  const [playingDeck, setPlayingDeck] = React.useState([]);
+  const [showCongratulationsModal, setShowCongratulationsModal] = React.useState(false);
+  const [showConfetti, setShowConfetti] = React.useState(false);
+  const [confettiComplete, setConfettiComplete] = React.useState(false);
+
+
+  const handleClick = (card) => {
+    if (attemptCards.size === 2 || attemptCards.has(card) || matchedCards.has(card)) {
+      return;
+    }
+    setAttemptCards((attemptCards) => new Set([...attemptCards, card]));
+    if (attemptCards.size === 1) {
+      const [firstCard, secondCard] = [...attemptCards, card];
+      if (firstCard instanceof PlayingCard && firstCard.equals(secondCard)) {
+        setMatchedCards(new Set([...matchedCards, firstCard, secondCard]));
+        setMatches(matches + 1);
+      }
+      setTimeout(() => {
+        setAttemptCards(new Set());
+      }, 1000);
+      setAttempts(attempts + 1);
+      if (matchedCards.size + 2 === playingDeck.length) {
+        setShowCongratulationsModal(true);
+        setShowConfetti(true);
+      }
+    }
+  };
+  
+  const handleYesClick = () => {
+    setShowCongratulationsModal(false);
+    setAttempts(0);
+    setMatches(0);
+    setMatchedCards(new Set());
+    setAttemptCards(new Set());
+    const cards = new Deck().shuffle().drawCards(10);
+    const clones = cards.map((card) => card.clone());
+    const dupes = [...cards, ...clones];
+    const deck = new Deck(dupes).shuffle().getCards();
+    setPlayingDeck(deck);
+  };
+
+  React.useEffect(() => {
+    const cards = new Deck().shuffle().drawCards(10);
+    const clones = cards.map((card) => card.clone());
+    const dupes = [...cards, ...clones];
+    const deck = new Deck(dupes).shuffle().getCards();
+    setPlayingDeck(deck);
+}, []);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Memory Game</h1>
+        <p>This game is just a little play around in React.</p>
+        <p>Match all the pairs of cards to win!</p>
+        <p>Click on a card to flip it. Try to find a match!</p>
+        <p>Number of pairs: 10</p>
+        <p>Number of attempts: {attempts}</p>
+        <p>Number of matches: {matches}</p>
+        <div className="card-list">
+          {playingDeck.map((card, index) => (
+          <>
+          <Card key={index} card={card} matchedCards={matchedCards} attemptCards={attemptCards} handleClick={handleClick} />
+          </>
+          ))}
+        </div>
+        {/* <ol>
+        {playingDeck.map((card, index) => (
+          <li key={index}>{card.toString()}</li>
+        ))}
+
+        </ol> */}
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          numberOfPieces={1000}
+          recycle={false}
+          run={showConfetti}
+          onConfettiComplete={() => {
+            setConfettiComplete(true); 
+            setShowConfetti(false);}
+          }
+        />
+        <Modal isOpen={showCongratulationsModal} onRequestClose={() => {
+          setShowCongratulationsModal(false);
+          setShowConfetti(false);
+        }}>
+          <h2>Congratulations!</h2>
+          <p>You won in {attempts} attempts.</p>
+          <p>Do you want to play again?</p>
+          <button
+            onClick={handleYesClick}
+          >
+            Yes
+          </button>
+          <button onClick={() => setShowCongratulationsModal(false)}>No</button>
+        </Modal>
       </header>
+      
     </div>
   );
 }
 
-export default App;
+export default React.memo(App);
---
+++ src/Deck.js
@@ -0,0 +1,159 @@
+/**
+ * Represents a deck of playing cards.
+ */
+export default class Deck {
+  /**
+   * Creates a new instance of the Deck class.
+   */
+  constructor(cards) {
+
+    const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+    const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+
+    if (cards) {
+      //update the id of the cards
+      for (let i = 0; i < cards.length; i++) {
+        cards[i].id = i;
+      }
+      this.cards = cards;
+    } else {
+      this.cards = [];
+      // generate a deck of cards
+      for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+          this.cards.push(new PlayingCard(suits[i], values[j], this.cards.length));
+        }
+      }
+    }
+  }
+
+  /**
+   * Shuffles the deck of cards.
+   * @returns {Array} The shuffled deck of cards.
+   */
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+    return this;
+  }
+
+  /**
+   * Returns the cards in the deck.
+   * @returns {Array} The cards in the deck.
+   */
+  getCards() {
+    return this.cards;
+  }
+
+  /**
+   * Draws a card from the deck.
+   * @returns {Card} The drawn card.
+   */
+  drawCard() {
+    return this.cards.pop();
+  }
+
+  /**
+   * Draws a number of cards from the deck.
+   * @param {number} count - The number of cards to draw.
+   * @returns {Array} The drawn cards.
+    */
+  drawCards(num) {
+    return this.cards.splice(0, num);
+  }
+}
+
+export class PlayingCard  {
+
+  /**
+   * Creates a new instance of the Card class.
+   * @param {string} suit - The suit of the card.
+   * @param {string} value - The value of the card.
+   */
+  constructor(suit, value, id) {
+    this.suit = suit;
+    this.value = value;
+    this.id = id;
+  }
+
+  /**
+   * Returns the suit of the card.
+   * @returns {string} The suit of the card.
+   */
+  getSuit() {
+    return this.suit;
+  }
+
+  /**
+   * Returns the value of the card.
+   * @returns {string} The value of the card.
+   */
+  getValue() {
+    return this.value;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  /**
+   * Returns a string representation of the card.
+   * @returns {string} A string representation of the card.
+   */
+  toString() {
+    return `${this.value} of ${this.suit}`;
+  }
+
+  /**
+   * Returns the symbol of the card's suit.
+   * @returns {string} The symbol of the card's suit.
+    */
+  getSuitSymbol() {
+    switch (this.suit) {
+      case "Hearts":
+        return "♥";
+      case "Spades":
+        return "♠";
+      case "Clubs":
+        return "♣";
+      case "Diamonds":
+        return "♦";
+      default:
+        return this.getSuit();
+    }
+  }
+
+  /**
+   * Returns a clone of the card.
+   * @returns {Card} A clone of the card.
+   * @example
+   * const card = new Card("Hearts", "Ace");
+   * const clone = card.clone();
+   * console.log(clone.getId()); // "Hearts-Ace-123456789"
+   * console.log(clone.getId() === card.getId()); // false
+   */
+  clone() {
+    return new PlayingCard(this.suit, this.value, this.id + 1);
+  }
+
+  /**
+   * Checks if the card is equal to another card.
+   * @param {Card} card - The card to compare.
+   * @returns {boolean} True if the cards are equal; otherwise, false.
+   * @example
+   * const card1 = new Card("Hearts", "Ace");
+   * const card2 = new Card("Hearts", "Ace");
+   * console.log(card1.equals(card2)); // true
+   * console.log(card1 === card2); // false
+   * 
+  */
+  equals(card) {
+    if (!(card instanceof PlayingCard)) {
+      return false;
+    }
+    return this.suit === card.getSuit() && this.value === card.getValue();
+  }
+
+}
